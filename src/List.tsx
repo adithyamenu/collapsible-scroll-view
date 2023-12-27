f93c1660ff67a9452132815f40db75d23f349922
adithyamenu <adithya_viswamithiran@menu.inc>
2023-12-27 15:42:00 +0900

    feat: synchronize tab scrolls

--- src/List.tsx
+++ src/List.tsx
@@ -1,17 +1,20 @@
-import React, { useCallback, useMemo } from "react";
+import React, { useCallback, useMemo, useRef } from "react";
 import { FlatList, StyleSheet, Text } from "react-native";
 import Animated, {
+  runOnJS,
+  useAnimatedReaction,
   useAnimatedScrollHandler,
-  useAnimatedStyle,
 } from "react-native-reanimated";
 import { useScrollableTabViewContext } from "./ScrollableTabViewProvider";
 import { HEADER_IMAGE_MINUS_INSET_HEIGHT, TAB_BAR_HEIGHT } from "./constants";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { debounce } from "lodash";
 
 const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
 
 export const List = React.memo(() => {
   const insets = useSafeAreaInsets();
+  const ref = useRef<FlatList>(null);
   const data = useMemo(() => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], []);
   const renderItem = useCallback(({ item }) => {
     return <Text style={styles.item}>{item}</Text>;
@@ -25,11 +28,36 @@ export const List = React.memo(() => {
     },
     []
   );
+  const debouncedScrollToOffset = useMemo(
+    () =>
+      debounce((result) => {
+        ref.current.scrollToOffset({ animated: false, offset: result });
+      }, 50),
+    []
+  );
+
+  const scrollToOffset = useCallback((result: number) => {
+    ref.current.scrollToOffset({ animated: false, offset: result });
+  }, []);
+  useAnimatedReaction(
+    () => scrollY.value,
+    (result, prev) => {
+      // worklet
+      if (result === prev) {
+        return;
+      }
+      runOnJS(debouncedScrollToOffset)(result);
+    },
+    [scrollToOffset]
+  );
   return (
     <AnimatedFlatList
+      ref={ref}
       data={data}
       renderItem={renderItem}
       onScroll={handleScroll}
+      refreshing={true}
+      onRefresh={() => {}}
       contentContainerStyle={[
         {
           paddingTop:
